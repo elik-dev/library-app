022e991f6cbfb2850c1434f594da6c39d160d5cd
Elik <130469789+elik-dev@users.noreply.github.com>
2023-07-24 20:42:48 +0600

    Set up skeleton HTML and file structure

---
+++ script.js
@@ -0,0 +1,30 @@
+let myLibrary = [];
+
+function Book() {
+
+}
+
+/*
+Requirements
+
+Script
+1. Has empty array for book objects
+2. Has book object constructor function
+3. Has add book object to library array function
+4. Has event handler function that brings up a form when add book button is clicked
+- Title
+- Author
+- Page count
+- Read status
+5. Has fill bookshelf function
+6. Has toggle read status function
+7. Has remove book object from an array
+- How to link DOM elements with array items?
+
+HTML
+1. Has add new book button (that brings up a form to input book details)
+2. Each book has:
+- Toggle read status button
+- Remove from library button
+3. Has grid/table container for book cards
+*/
