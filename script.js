30176f06db0423bf9ee5285fcb5b29041b35e774
Elik <130469789+elik-dev@users.noreply.github.com>
2024-01-03 20:26:04 +0600

    Add dialog popup function

--- script.js
+++ script.js
@@ -22,24 +22,29 @@ function Book(title, author, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(event) {
+
 }
 
-function refreshBookShelf() {
+function refreshBookShelf(event) {
     const bookshelf = document.querySelector(".bookshelf");
     myLibrary.forEach(book => {
         const bookCard = document.createElement("div");
-        bookCard.classList.add("book")
+        bookCard.classList.add("book-card")
+        bookCard.setAttribute("data-index", `${myLibrary.indexOf(book)}`)
+
         const bookTitle = document.createElement("h2");
         bookTitle.classList.add("book-title");
         bookTitle.textContent = book.title;
+
         const bookAuthor = document.createElement("p");
         bookAuthor.classList.add("book-author");
         bookAuthor.textContent = book.author;
+
         const readStatus = document.createElement("p");
-        readStatus.classList.add("read-status");
+        readStatus.classList.add("book-read-status");
         readStatus.textContent = book.isRead ? "Read" : "Not read yet";
+
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(readStatus);
@@ -47,16 +52,25 @@ function refreshBookShelf() {
     });
 }
 
+function showNewBookDialog(event) {
+    const form = document.createElement("form");
+    form.classList.add("new-book-form")
+    const titleLabel = document.createElement("label");
+    const titleInput = document.createElement("input");
+    const authorLabel = document.createElement("label");
+    const authorInput = document.createElement("input");
+    const readStatusLabel = document.createElement("label");
+    const readStatusInput = document.createElement("input");
+}
+
+function hideNewBookDialog(event) {
+
+}
+
+const newBookButton = document.querySelector(".new-book-btn");
+newBookButton.addEventListener("click", createBookDialog);
 window.addEventListener("load", refreshBookShelf);
 
 /*
-Requirements
-1. Has empty array for book objects
-2. Has book object constructor function
-3. Has add book object to library array function
-4. Has button that brings up a form for new book
-5. Book cards have remove from library button
-
-
 
 */
