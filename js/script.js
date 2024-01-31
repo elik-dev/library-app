96def2443ff577f1c967862bb8163d62dac13376
elik-dev <130469789+elik-dev@users.noreply.github.com>
2024-01-31 21:50:20 +0600

    Set up skeleton script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+function Book(title, author, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+    // create form to get user input
+    // create book object based on user input
+    // create remove book button and attach it to the book
+    // add book object to the library array and to the page
+}
+
+function buildBookshelf() {
+    // iterate through the library array and add books to the page
+    // create new book button
+}
+
+function removeBookFromLibrary() {
+    // remove book from the library array and from the page
+}
+
+function toggleReadStatus() {
+    // change read status of the book
+}
